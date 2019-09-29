f58e6c8a0d4e7e244261144a4df2e1aeaf27dce1
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-09-29 17:59:28 -0400

    generate outcome for new cohort

--- Code/DataPrep/outcome/pull_outcome.py
+++ Code/DataPrep/outcome/pull_outcome.py
@@ -6,16 +6,16 @@ from utils.db_utils import create_connection, create_table, export_table
 def main():
     database = "../../../Data/db/adult_decomp.db"
  
-    outcome_qry = open('./outcome_1st_icu_admission.sql', 'r').read()
+    outcome_qry = open('./sql/outcome.sql', 'r').read()
  
     # create a database connection
     conn = create_connection(database)
     if conn is not None:
-        # create adult_decomp_cohort table
+        # create outcome table
         create_table(conn, outcome_qry)
     else:
         print("Error! cannot create the database connection.")
-    export_table(conn, 'outcome_1st_icu_admission', '../../../Data/Processed/outcome/outcome_1st_icu_admission.csv')
+    export_table(conn, 'outcome', '../../../Data/Processed/outcome/outcome.csv')
 
     conn.close()
 
