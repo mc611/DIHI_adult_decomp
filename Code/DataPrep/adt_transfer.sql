21ce8708c07950198ac5171d7a4ad0fd42b09863
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-11-04 21:16:34 -0500

    update transfer table

--- Code/DataPrep/adt_transfer.sql
+++ Code/DataPrep/adt_transfer.sql
@@ -1,8 +1,31 @@
--- This script extracts all the transfer events from the adt events raw data, and categorize these events 
+-- Adult Decompensation Prediction
+--
+-- Copyright 2019 Ziyuan Shen, Duke Institute for Health Innovation (DIHI), Duke University School of Medicine, Durham NC.
+--
+-- All Rights Reserved.
+
+-- This script extracts all the transfer events from the adt events raw data, and categorize these events
+
 DROP TABLE IF EXISTS adult_decomp_adt_transfer;
 CREATE TABLE adult_decomp_adt_transfer AS
+-- categorize hospital departments into ED, floor, stepdown, surgery and ICU
+WITH hospital_units AS
+(
+  SELECT department_name
+    , level_of_care
+    , specialty
+    , start_dttm
+    , end_dttm
+    , CASE WHEN level_of_care='intensive care' THEN 'ICU'
+           WHEN specialty='emergency' THEN 'ED'
+           WHEN specialty='surgery' THEN 'surgery'
+           WHEN level_of_care='regular' AND ((specialty<>'emergency' AND specialty<>'surgery') OR specialty IS NULL) THEN 'floor'
+           WHEN level_of_care='stepdown' AND (specialty<>'surgery' OR specialty IS NULL) THEN 'stepdown'
+        ELSE 'Other' END AS unit_category
+  FROM ad_hospital_units
+)
 -- create an intermediate table which stores all transfer events' in-out departments
-WITH transfer_in_out AS
+, transfer_in_out AS
 (
   SELECT pat_id
     , pat_enc_csn_id
@@ -35,13 +58,15 @@ WITH transfer_in_out AS
     , trans.transfer_out_dep
     , trans_out.level_of_care AS transfer_out_level_of_care
     , trans_out.specialty AS transfer_out_specialty
+    , trans_out.unit_category as transfer_out_unit_category
     , trans.transfer_in_dep
     , trans_in.level_of_care AS transfer_in_level_of_care
     , trans_in.specialty AS transfer_in_specialty
+    , trans_in.unit_category AS transfer_in_unit_category
   FROM transfer_in_out_2 AS trans
-  LEFT JOIN ad_hospital_units AS trans_out
+  LEFT JOIN hospital_units AS trans_out
     ON trans.transfer_out_dep = trans_out.department_name
-  LEFT JOIN ad_hospital_units AS trans_in
+  LEFT JOIN hospital_units AS trans_in
     ON trans.transfer_in_dep = trans_in.department_name
   WHERE trans.transfer_dttm >= trans_out.start_dttm
     AND trans.transfer_dttm <= trans_out.end_dttm
@@ -58,12 +83,5 @@ SELECT pat_id
   , transfer_in_dep
   , transfer_in_level_of_care
   , transfer_in_specialty
-  , CASE WHEN transfer_out_specialty='emergency' AND transfer_in_level_of_care='intensive care' THEN 'ED to ICU'
-         WHEN transfer_out_level_of_care='regular' AND (transfer_out_specialty<>'emergency' OR transfer_out_specialty IS NULL) AND transfer_in_level_of_care='intensive care' THEN 'floor to ICU'
-         WHEN transfer_out_level_of_care='stepdown' AND transfer_in_level_of_care='intensive care' THEN 'stepdown to ICU'
-         WHEN transfer_out_level_of_care='regular' AND (transfer_out_specialty<>'emergency' OR transfer_out_specialty IS NULL) AND transfer_in_level_of_care='stepdown' THEN 'floor to stepdown'
-         WHEN transfer_out_level_of_care='intensive care' AND transfer_in_level_of_care='regular' AND (transfer_in_specialty<>'emergency' OR transfer_in_specialty IS NULL) THEN 'ICU to floor'
-         WHEN transfer_out_level_of_care='intensive care' AND transfer_in_level_of_care='stepdown' THEN 'ICU to stepdown'
-         WHEN transfer_out_level_of_care='stepdown' AND transfer_in_level_of_care='regular' AND (transfer_in_level_of_care<>'emergency' OR transfer_in_specialty IS NULL) THEN 'stepdown to floor'
-      ELSE 'Other' END AS transfer_type
+  , transfer_out_unit_category || ' to ' || transfer_in_unit_category AS transfer_type
 FROM transfer_units;
